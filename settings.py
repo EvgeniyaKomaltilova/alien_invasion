80786e2ef6408d56c5821cbbdfe94fde71ce3793
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-05-22 16:24:03 +0700

    screen, ship

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Класс для хранения всех настроек игры"""
+    def __init__(self):
+        """Изициализирует настройки игры"""
+        # Параметры экрана
+        self.screen_widht = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
